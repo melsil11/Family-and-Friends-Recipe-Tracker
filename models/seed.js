9a9f17ba8b1e120d790598c0afdf5a66fafdd3b2
melanie silva <melsil11@hotmail.com>
2022-10-11 11:56:47 -0400

    started liquid views, fixed boolean seed data

--- models/seed.js
+++ models/seed.js
@@ -6,18 +6,16 @@ const db = mongoose.connection
 db.on('open', () => {
 
     const startRecipes =[
-        {name: 'Cal De Galinga',planBased: false, vegetarian: false, dairyFree: true,
-        hasMeat: true,
-        glutenFree: true,},
-        {name: 'Chicken Continental', planBased:'false', vegetarian:'false', dairyFree: 'false', hasMeat: 'true', glutenFree:'false' },
-        {name: 'Apple Pie', planBased:'false', vegetarian:'true', dairyFree: 'false', hasMeat: 'false', glutenFree:'false' },
-        {name: 'Lasagna', planBased:'false', vegetarian:'false', dairyFree: 'false', hasMeat: 'true', glutenFree:'false' },
-        {name: 'Nestle Chocolate Chip Cookies', planBased:'false', vegetarian:'true', dairyFree: 'false', hasMeat: 'false', glutenFree:'false' },
-        {name: 'Brownies', planBased:'false', vegetarian:'true', dairyFree: 'false', hasMeat: 'false', glutenFree:'false' },
-        {name: 'VegetarianTeriyaki Stir Fry', planBased:'true', vegetarian:'true', dairyFree: 'true', hasMeat: 'false', glutenFree:'true' },
-        {name: 'Sweet Potato Black Bean Chili', planBased:'true', vegetarian:'true', dairyFree: 'true', hasMeat: 'false', glutenFree:'true' },
-        {name: 'Black Bean Brownies', planBased:'true', vegetarian:'true', dairyFree: 'true', hasMeat: 'false', glutenFree:'false' },
-        {name: 'Vegan Cornbread', planBased:'true', vegetarian:'true', dairyFree: 'true', hasMeat: 'false', glutenFree:'false' },
+        { name: 'Cal De Galinga', planBased: false, vegetarian: false, dairyFree: true, hasMeat: true, glutenFree: true },
+        { name: 'Chicken Continental', planBased:false, vegetarian:false, dairyFree: false, hasMeat: true, glutenFree:false },
+        { name: 'Apple Pie', planBased:false, vegetarian:true, dairyFree: false, hasMeat: false, glutenFree:false },
+        { name: 'Lasagna', planBased:false, vegetarian:false, dairyFree: false, hasMeat: true, glutenFree:false },
+        { name: 'Nestle Chocolate Chip Cookies', planBased:false, vegetarian:true, dairyFree: false, hasMeat: false, glutenFree: false },
+        { name: 'Brownies', planBased:false, vegetarian:true, dairyFree: false, hasMeat: false, glutenFree:false },
+        { name: 'VegetarianTeriyaki Stir Fry', planBased:true, vegetarian:true, dairyFree: true, hasMeat: false, glutenFree:true },
+        { name: 'Sweet Potato Black Bean Chili', planBased:true, vegetarian:true, dairyFree: true, hasMeat: false, glutenFree: true },
+        { name: 'Black Bean Brownies', planBased:true, vegetarian:true, dairyFree: true, hasMeat: false, glutenFree:false },
+        { name: 'Vegan Cornbread', planBased:true, vegetarian:true, dairyFree: true, hasMeat: false, glutenFree:false },
     ]
 
 Recipe.deleteMany({})
